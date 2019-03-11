fbb0ac343e27ef0837387341de89a6e3781c4626
kirilcvetkov92 <kirilcvetkov92@gmail.com>
2019-03-11 18:41:15 +0100

    Added cityscape helper

--- cityscapes_helper.py
+++ cityscapes_helper.py
@@ -19,6 +19,8 @@ def get_label_info():
     # Returns
         Two lists: one for the class names and the other for the label values
     """
+
+    # a label and all meta information
     Label = namedtuple('Label', [
 
         'name',  # The identifier of this label, e.g. 'car', 'person', ... .
@@ -57,17 +59,50 @@ def get_label_info():
 
     labels = [
         #       name                     id    trainId   category            catId     hasInstances   ignoreInEval   color
-
+        Label('unlabeled', 0, 255, 'void', 0, False, True, (0, 0, 0)),
+        Label('ego vehicle', 1, 255, 'void', 0, False, True, (0, 0, 0)),
+        Label('rectification border', 2, 255, 'void', 0, False, True, (0, 0, 0)),
+        Label('out of roi', 3, 255, 'void', 0, False, True, (0, 0, 0)),
+        Label('static', 4, 255, 'void', 0, False, True, (0, 0, 0)),
+        Label('dynamic', 5, 255, 'void', 0, False, True, (111, 74, 0)),
+        Label('ground', 6, 255, 'void', 0, False, True, (81, 0, 81)),
         Label('road', 7, 0, 'flat', 1, False, False, (128, 64, 128)),
-
+        Label('sidewalk', 8, 1, 'flat', 1, False, False, (244, 35, 232)),
+        Label('parking', 9, 255, 'flat', 1, False, True, (250, 170, 160)),
+        Label('rail track', 10, 255, 'flat', 1, False, True, (230, 150, 140)),
+        Label('building', 11, 2, 'construction', 2, False, False, (70, 70, 70)),
+        Label('wall', 12, 3, 'construction', 2, False, False, (102, 102, 156)),
+        Label('fence', 13, 4, 'construction', 2, False, False, (190, 153, 153)),
+        Label('guard rail', 14, 255, 'construction', 2, False, True, (180, 165, 180)),
+        Label('bridge', 15, 255, 'construction', 2, False, True, (150, 100, 100)),
+        Label('tunnel', 16, 255, 'construction', 2, False, True, (150, 120, 90)),
+        Label('pole', 17, 5, 'object', 3, False, False, (153, 153, 153)),
+        Label('polegroup', 18, 255, 'object', 3, False, True, (153, 153, 153)),
+        Label('traffic light', 19, 6, 'object', 3, False, False, (250, 170, 30)),
+        Label('traffic sign', 20, 7, 'object', 3, False, False, (220, 220, 0)),
+        Label('vegetation', 21, 8, 'nature', 4, False, False, (107, 142, 35)),
+        Label('terrain', 22, 9, 'nature', 4, False, False, (152, 251, 152)),
+        Label('sky', 23, 10, 'sky', 5, False, False, (70, 130, 180)),
+        Label('person', 24, 11, 'human', 6, True, False, (220, 20, 60)),
+        Label('rider', 25, 12, 'human', 6, True, False, (255, 0, 0)),
         Label('car', 26, 13, 'vehicle', 7, True, False, (0, 0, 142)),
+        Label('truck', 27, 14, 'vehicle', 7, True, False, (0, 0, 70)),
+        Label('bus', 28, 15, 'vehicle', 7, True, False, (0, 60, 100)),
+        Label('caravan', 29, 255, 'vehicle', 7, True, True, (0, 0, 90)),
+        Label('trailer', 30, 255, 'vehicle', 7, True, True, (0, 0, 110)),
+        Label('train', 31, 16, 'vehicle', 7, True, False, (0, 80, 100)),
+        Label('motorcycle', 32, 17, 'vehicle', 7, True, False, (0, 0, 230)),
+        Label('bicycle', 33, 18, 'vehicle', 7, True, False, (119, 11, 32)),
+        Label('license plate', -1, -1, 'vehicle', 7, False, True, (0, 0, 142)),
     ]
 
+
     class_names = []
-    label_values = []
-    for label in labels:
+    label_values = list(set(map(lambda x: x[7], labels)))
+
+    for label in label_values:
         class_names.append(label[0])
-        label_values.append(label[7])
+        label_values.append(label[1])
 
     return class_names, label_values
 
@@ -86,9 +121,9 @@ def one_hot_it(label, label_values):
     """
 
     semantic_map = []
-    c = np.logical_and(np.not_equal(label, label_values[0]), np.not_equal(label, label_values[1]))
-    mask = np.any(c, axis=-1)
-    semantic_map.append(mask)
+    # c = np.logical_and(np.not_equal(label, label_values[0]), np.not_equal(label, label_values[1]))
+    # mask = np.any(c, axis=-1)
+    # semantic_map.append(mask)
     for colour in label_values:
         equality = np.equal(label, colour)
         class_map = np.all(equality, axis=-1)
@@ -143,63 +178,10 @@ def load_annotation(path):
     return image
 
 
-def get_data():
-    PATH = 'D:/data/leftImg8bit_trainvaltest/'
-    train_path = PATH + '/leftImg8bit/train/'
-    trainy_path = PATH + '/sky-data/train/'
-
-    val_path = PATH + '/leftImg8bit/val/'
-    valy_path = PATH + '/sky-data/val/'
-
-    train_batch = glob(os.path.join(train_path, '*/*.png'))
-
-    trainy_batch = glob(os.path.join(trainy_path, '*/*color.png'))
-    val_batch = glob(os.path.join(val_path, '*/*.png'))
-
-    valy_batch = glob(os.path.join(valy_path, '*/*color.png'))
-
-    X_train = []
-    y_train = []
-    X_val = []
-    y_val = []
-
-    print('Loading X_Train..')
-    for sample in train_batch:
-        img_path = sample
-        x = load_image(img_path)
-        x = cv2.resize(x, dsize=(512, 256))
-        X_train.append(x)
-
-    print('Loading Y_Train..')
-    for sample in trainy_batch:
-        img_path = sample
-        x = load_image(img_path)
-        x = cv2.resize(x, dsize=(512, 256))
-        y_train.append(x)
-
-    print('Loading X_Validation..')
-    for sample in val_batch:
-        img_path = sample
-        x = load_image(img_path)
-        x = cv2.resize(x, dsize=(512, 256))
-        X_val.append(x)
-
-    print('Loading Y_Validation..')
-    for sample in valy_batch:
-        img_path = sample
-        x = load_image(img_path)
-        x = cv2.resize(x, dsize=(512, 256))
-        y_val.append(x)
-
-    return X_train, y_train, X_val, y_val
-
-
-
-
 def flip_image(image, measurement, flip_probability=1.0):
     if random.random() <= flip_probability:
         image = cv2.flip(image, 1)
-        measurement*=-1
+        measurement *= -1
     return image, measurement
 
 
@@ -218,18 +200,17 @@ def random_crop(image, label, crop_height, crop_width):
             return image[y:y + crop_height, x:x + crop_width, :], label[y:y + crop_height, x:x + crop_width]
     else:
         raise Exception('Crop shape (%d, %d) exceeds image dimensions (%d, %d)!' % (
-        crop_height, crop_width, image.shape[0], image.shape[1]))
-
+            crop_height, crop_width, image.shape[0], image.shape[1]))
 
 
 def random_shadow(image):
     """
     Generates and adds random shadow
     """
-    rand_width_scal_1 =  np.random.rand()
-    IMAGE_WIDTH, IMAGE_HEIGHT = image.shape
-    x1, y1 = IMAGE_WIDTH *  rand_width_scal_1, 0
-    rand_width_scal_2 =  np.random.rand()
+    rand_width_scal_1 = np.random.rand()
+    IMAGE_HEIGHT, IMAGE_WIDTH, _ = image.shape
+    x1, y1 = IMAGE_WIDTH * rand_width_scal_1, 0
+    rand_width_scal_2 = np.random.rand()
     x2, y2 = IMAGE_WIDTH * rand_width_scal_2, IMAGE_HEIGHT
     xn, yn = np.mgrid[0:IMAGE_HEIGHT, 0:IMAGE_WIDTH]
     mask = np.zeros_like(image[:, :, 1])
@@ -242,6 +223,13 @@ def random_shadow(image):
     hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
     return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
 
+def bc_img(img, contrast=1.0, bright=0.0):
+    img = img.astype(np.int)
+    img = img * contrast + bright
+    img[img > 255] = 255
+    img[img < 0] = 0
+    img = img.astype(np.uint8)
+    return img
 
 def random_brightness(image):
     """
@@ -252,34 +240,29 @@ def random_brightness(image):
     hsv_channel = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
     brightness_scalar = np.random.rand()
     ratio = 1.0 + 0.4 * (brightness_scalar - 0.5)
-    hsv_channel[:,:,2] =  hsv_channel[:,:,2] * ratio
+    hsv_channel[:, :, 2] = hsv_channel[:, :, 2] * ratio
     return cv2.cvtColor(hsv_channel, cv2.COLOR_HSV2RGB)
 
 
-def bc_img(img, s = 1.0, m = 0.0):
-    img = img.astype(np.int)
-    img = img * s + m
-    img[img > 255] = 255
-    img[img < 0] = 0
-    img = img.astype(np.uint8)
-    return img
 
 
+def rotation(input_image, output_image, degrees):
+    angle = random.uniform(-1*degrees, degrees)
+    M = cv2.getRotationMatrix2D((input_image.shape[1]//2, input_image.shape[0]//2), angle, 1.0)
+    input_image = cv2.warpAffine(input_image, M, (input_image.shape[1], input_image.shape[0]), flags=cv2.INTER_NEAREST)
+    output_image = cv2.warpAffine(output_image, M, (output_image.shape[1], output_image.shape[0]), flags=cv2.INTER_NEAREST)
+    return input_image, output_image
+
+def brightness(image, bright_0_1):
+            factor = 1.0 + random.uniform(-1.0*bright_0_1, bright_0_1)
+            table = np.array([((i / 255.0) * factor) * 255 for i in np.arange(0, 256)]).astype(np.uint8)
+            input_image = cv2.LUT(input_image, table)
 def data_augmentation(input_image, output_image):
-    # Data augmentation
-    # try:
-    #     random_crop(image, label, 320, 240)
-    # except:
-    #     pass
-    # go here
-    t = random.uniform(0.85, 1.15)  # Contrast augmentation
-    c = random.randint(-40, 30)  # Brightness augmentation
-    input_image = bc_img(input_image, t, c)
-    # if  random.randint(0,1):
-    #     angle = random.uniform(-1*45, 45)
-    #     M = cv2.getRotationMatrix2D((input_image.shape[1]//2, input_image.shape[0]//2), angle, 1.0)
-    #     input_image = cv2.warpAffine(input_image, M, (input_image.shape[1], input_image.shape[0]), flags=cv2.INTER_NEAREST)
-    #     output_image = cv2.warpAffine(output_image, M, (output_image.shape[1], output_image.shape[0]), flags=cv2.INTER_NEAREST)
+
+    contrast = random.uniform(0.8, 1)  # Contrast augmentation
+    bright = random.randint(-40, 30)  # Brightness augmentation
+    input_image = bc_img(input_image, contrast, bright)
+
 
     return input_image, output_image
 
@@ -297,7 +280,7 @@ def gen_batch_function(samplesX, samplesY, label_values, batch_size=1, is_train=
 
     num_samples = len(samplesX)
 
-    if(batch_size==-1):
+    if (batch_size == -1):
         batch_size = num_samples
 
     # Loop through batches and grab images, yielding each batch
@@ -314,7 +297,6 @@ def gen_batch_function(samplesX, samplesY, label_values, batch_size=1, is_train=
                 x, y = data_augmentation(x, y)
 
             y = np.float32(one_hot_it(y, label_values=label_values))
-
             X_f.append(x)
             y_f.append(y)
 
--- cityscapes_main.py
+++ cityscapes_main.py
@@ -45,7 +45,8 @@ def load_vgg(sess, vgg_path):
 
 tests.test_load_vgg(load_vgg, tf)
 
-import keras
+
+
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
     Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
@@ -57,30 +58,44 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
     # TODO: Implement function
     layer7_conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, 1,
-                                       padding='same',  kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG), name='conv_1_1_1',activation = tf.nn.relu)
 
-                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+   # layer7_conv_1x1 = tf.layers.batch_normalization(layer7_conv_1x1)
+
     output = tf.layers.conv2d_transpose(layer7_conv_1x1, num_classes, 4, 2,
-                                        padding='same', kernel_initializer= tf.random_normal_initializer(stddev=0.01),
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                        padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG), name='conv_1_1_2',activation = tf.nn.relu)
+    #output = keras.layers.UpSampling2D(size=(2,2),data_format=None,interpolation='bilinear')(layer7_conv_1x1)
+
     layer4_conv_1x1 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, 1,
-                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=0.01),
-                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    output = tf.add(output, layer4_conv_1x1)
-    #output = keras.layers.UpSampling2D(size=(2,2),     data_format=None,interpolation='bilinear')(output)
+                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG), name='conv_1_1_3',activation = tf.nn.relu)
+    #layer4_conv_1x1 = tf.layers.batch_normalization(layer4_conv_1x1)
+
+    output = tf.add(output, layer4_conv_1x1, name='conv_1_1_4')
+    #output = tf.layers.batch_normalization(output)
+
+    #output = keras.layers.UpSampling2D(size=(2,2),data_format=None,interpolation='bilinear')(output)
 
 
     output = tf.layers.conv2d_transpose(output, num_classes, 4, 2,
-                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=0.01),
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG),  name='conv_1_1_5',activation = tf.nn.relu)
     layer3_conv_1x1 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, 1,
-                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=0.01),
-                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    output = tf.add(output, layer3_conv_1x1)
+                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG), name='conv_1_1_6',activation = tf.nn.relu)
+    output = tf.add(output, layer3_conv_1x1,  name='conv_1_1_7')
+   # output = tf.layers.batch_normalization(output)
+
+    #output = keras.layers.UpSampling2D(size=(8,8),data_format=None,interpolation='bilinear')(output)
     output = tf.layers.conv2d_transpose(output, num_classes, 16, 8,
-                                        padding='same', kernel_initializer= tf.random_normal_initializer(stddev=0.01),
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                        padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG),  name='conv_1_1_8',activation = tf.nn.relu)
+
     return output
+
+
 tests.test_layers(layers)
 
 
@@ -95,17 +110,31 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     # TODO: Implement function
 
-    logits = tf.reshape(nn_last_layer, (-1, num_classes))
-    labels = tf.reshape(correct_label, (-1, num_classes))
+    logits = tf.reshape(nn_last_layer, (-1, num_classes), name='logits')
+    labels = tf.reshape(correct_label, (-1, num_classes), name='labels')
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
-    train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+    softmax = tf.nn.softmax(logits, name='softmax')
+
+    # global_step = tf.Variable(0, trainable=False)
+
+    #     train_op = tf.train.AdamOptimizer(learning_rate=
+    #                                       cyclic_learning_rate(global_step=global_step,
+    #                                                                mode='triangular2')).minimize(cross_entropy_loss,global_step=global_step)
+    train_op = tf.train.AdamOptimizer(learning_rate=0.0005).minimize(cross_entropy_loss)
+
+    #     gradients = optimizer.compute_gradients(cross_entropy_loss)
+    #     capped_gvs = [(tf.clip_by_value(grad, -.5, .5), var) for grad, var in gradients]
+    #     train_op = optimizer.apply_gradients(capped_gvs)
+
     return logits, train_op, cross_entropy_loss
 
 tests.test_optimize(optimize)
 
 import time
+
+
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
-             correct_label, keep_prob, learning_rate):
+                 correct_label, keep_prob, learning_rate, X_train, y_train, label_values, X_val, y_val):
     """
     Train neural network and print out the loss during training.
     :param sess: TF Session
@@ -119,54 +148,75 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    # TODO: Implement function
+    image_shape = (160, 576)  # KITTI dataset uses 160x576 images
+    data_dir = './data'
+    runs_dir = './runs'
+
+    mean_loss = []
     for epoch in range(epochs):
-        for image, targets in get_batches_fn(batch_size):
+        print('epoch : ', epoch)
+        for image, targets in get_batches_fn(X_train, y_train, label_values, batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
-                               feed_dict={input_image: image, correct_label: targets, keep_prob: 0.5,
-                                          learning_rate: 0.001})
+                               feed_dict={input_image: image, correct_label: targets, keep_prob: KEEP_PROB,
+                                          learning_rate: LEARNING_RATE})
+        #         assign_op = global_step.assign(epoch)
+        #         sess.run(assign_op)
         # Print data on the learning process
+
         print("Epoch: {}".format(epoch + 1), "/ {}".format(epochs), " Loss: {:.3f}".format(loss))
+
+        for image, targets in get_batches_fn(X_val, y_val, label_values, batch_size, is_train=False):
+            loss = sess.run([cross_entropy_loss],
+                            feed_dict={input_image: image, correct_label: targets, keep_prob: 1})
+            mean_loss.append(loss)
+
+        mean_loss_ = np.mean(np.array(mean_loss))
+        print("Epoch: {}".format(epoch + 1), "/ {}".format(epochs), " Validation Loss: {:.3f}".format(mean_loss_))
+
+        print('saving model')
+        saver.save(sess, './model')
+
 
 tests.test_train_nn(train_nn)
 
 
 def run():
-    num_classes = 2
+    num_classes = 29
     image_shape = (160, 576)  # KITTI dataset uses 160x576 images
     data_dir = './data'
     runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
-    epochs = 100
-    batch_size = 10
+    epochs = 30
+    batch_size = 16
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
 
-    # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
-    # You'll need a GPU with at least 10 teraFLOPS to train on.
-    #  https://www.cityscapes-dataset.com/
+    print('run')
 
+    tf.reset_default_graph()
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+        get_batches_fn = gen_batch_function
 
         input, keep_prob, layer3, layer4, layer7 = load_vgg(sess, vgg_path)
-        output = layers(layer3, layer4, layer7, 2)
-        correct_label = tf.placeholder(dtype=tf.float32, shape=(None, None, None, 2))
+        print(input)
+        output = layers(layer3, layer4, layer7, num_classes)
+        correct_label = tf.placeholder(dtype=tf.float32, shape=(None, None, None, num_classes))
         learning_rate = tf.placeholder(dtype=tf.float32)
-        logits, train_op, cross_entropy_loss = optimize(output, correct_label, learning_rate, 2)
-        tf.set_random_seed(123)
+        logits, train_op, cross_entropy_loss = optimize(output, correct_label, learning_rate, num_classes)
         sess.run(tf.global_variables_initializer())
+
         saver = tf.train.Saver()  # Simple model saver
 
+        # saver.restore(sess, tf.train.latest_checkpoint('.'))
 
-        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input, correct_label,
-                 keep_prob, learning_rate)
+        train_nn_sky(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input, correct_label,
+                     keep_prob, learning_rate, X_train, y_train, label_values, X_val, y_val)
+
         # Save inference data using helper.save_inference_samples
-        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input)
+        # helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input)
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
