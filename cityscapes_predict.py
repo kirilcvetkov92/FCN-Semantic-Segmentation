e7c0b4a651bc38528906ea69127993699a52a9f1
kirilcvetkov92 <kirilcvetkov92@gmail.com>
2019-03-12 22:02:15 +0100

    Added notebooks and predict script

--- cityscapes_predict.py
+++ cityscapes_predict.py
@@ -7,43 +7,97 @@ from distutils.version import LooseVersion
 import project_tests as tests
 import cityscapes_helper
 import numpy as np
+import argparse as parser
+from cityscapes_config import *
+from cityscapes_helper import *
+from moviepy.editor import VideoFileClip
+import cv2
 
-label_values = cityscapes_helper.get_label_info()
-
-class_names, label_values = cityscapes_helper.get_label_info()
-L2_REG = 1e-6
-STDEV = 1e-3
-KEEP_PROB = 0.5
-LEARNING_RATE = 1e-4
-
-
-def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
+def load_image(path):
+    image = cv2.cvtColor(cv2.imread(path, -1), cv2.COLOR_BGR2RGB)
+    return image
 
 
-    logits = tf.reshape(nn_last_layer, (-1, num_classes), name='logits')
-    labels = tf.reshape(correct_label, (-1, num_classes), name='labels')
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
-    softmax = tf.nn.softmax(logits, name='softmax')
+def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+    """
+    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
+    :param vgg_layer3_out: TF Tensor for VGG Layer 3 output
+    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
+    :param vgg_layer7_out: TF Tensor for VGG Layer 7 output
+    :param num_classes: Number of classes to classify
+    :return: The Tensor for the last layer of output
+    """
+    # TODO: Implement function
+    l7_conv = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, 1,
+                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG), name='conv_1_1_1',activation = tf.nn.relu)
 
-    train_op = tf.train.AdamOptimizer(learning_rate=0.0005).minimize(cross_entropy_loss)
 
+    conv1 = tf.layers.conv2d_transpose(l7_conv, num_classes, 4, 2,
+                                        padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG), name='conv_1_1_2',activation = tf.nn.relu)
 
-    return logits, train_op, cross_entropy_loss
+    l4_conv = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, 1,
+                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG), name='conv_1_1_3',activation = tf.nn.relu)
+
+    skip_1 = tf.add(conv1, l4_conv, name='conv_1_1_4')
+    #output = tf.layers.batch_normalization(output)
+    #output = keras.layers.UpSampling2D(size=(2,2),data_format=None,interpolation='bilinear')(output)
+
+    conv2 = tf.layers.conv2d_transpose(skip_1, num_classes, 4, 2,
+                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG),  name='conv_1_1_5',activation = tf.nn.relu)
+    l3_conv = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, 1,
+                                       padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG), name='conv_1_1_6',activation = tf.nn.relu)
+    skip_3 = tf.add(conv2, l3_conv,  name='conv_1_1_7')
+
+    output = tf.layers.conv2d_transpose(skip_3, num_classes, 16, 8,
+                                        padding='same', kernel_initializer= tf.random_normal_initializer(stddev=STDEV),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(L2_REG),  name='conv_1_1_8',activation = tf.nn.relu)
+
+    return output
+
+def load_vgg(sess, vgg_path):
+    """
+    Load Pretrained VGG Model into TensorFlow.
+    :param sess: TensorFlow Session
+    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
+    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+    """
+    # TODO: Implement function
+    #   Use tf.saved_model.loader.load to load the model and weights
+    vgg_tag = 'vgg16'
+    vgg_input_tensor_name = 'image_input:0'
+    vgg_keep_prob_tensor_name = 'keep_prob:0'
+    vgg_layer3_out_tensor_name = 'layer3_out:0'
+    vgg_layer4_out_tensor_name = 'layer4_out:0'
+    vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    graph = tf.get_default_graph()
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    return input, keep_prob, layer3, layer4, layer7
 
 
 def pipeline_final(img, is_video):
     channel = 1 if is_video else 4
-    size = img.shape
+
     img = cv2.resize(img, dsize=(512, 256))
     img = np.array([img])
-    softmax_ = loss = sess.run([softmax],
+    softmax_   = sess.run([softmax],
                                feed_dict={input: img, keep_prob: 1})
     logits_ = (softmax_[0].reshape(1, 256, 512, 29))
     output_image = reverse_one_hot(logits_[0])
 
     print(output_image.shape)
 
-    out_vis_image = colour_code_segmentation(output_image, c_values)
+    out_vis_image = colour_code_segmentation(output_image, label_values)
 
     a = cv2.cvtColor(np.uint8(out_vis_image), channel)
 
@@ -60,24 +114,24 @@ def pipeline_video(img):
 def pipeline_img(img):
     return pipeline_final(img, False)
 
-def process(media_dir, save_dir, model_dir):
-    global model, label_values
+def process(media_dir, save_dir):
+    global sess, softmax, label_values, input, keep_prob
 
     data_dir = './data'
     num_classes = 29
+
+    label_values = cityscapes_helper.get_label_info()
+
     tf.reset_default_graph()
     sess = tf.Session()
 
     # Path to vgg model
     vgg_path = os.path.join(data_dir, 'vgg')
     # Create function to get batches
-    get_batches_fn = gen_batch_function
 
     input, keep_prob, layer3, layer4, layer7 = load_vgg(sess, vgg_path)
-    print(input)
     output = layers(layer3, layer4, layer7, num_classes)
 
-    tf.set_random_seed(123)
     sess.run(tf.global_variables_initializer())
 
     saver = tf.train.Saver()  # Simple model saver
@@ -87,10 +141,6 @@ def process(media_dir, save_dir, model_dir):
     logits = tf.reshape(output, (-1, num_classes))
     softmax = tf.nn.softmax(logits, name='softmax')
 
-
-
-    model = load_model(model_dir, custom_objects={'preprocess_input': preprocess_input})
-    label_values, _, _ = get_label_values()
 
     try:
         img = load_image(media_dir)
@@ -121,5 +171,4 @@ if __name__ == '__main__':
 
         crawler = process(media_dir=parsed_arg.media_dir,
                           save_dir=parsed_arg.save_dir,
-                          model_dir = parsed_arg.model_dir
                           )
